874a14c7d53d81fb769895d75e92c45349be52e1
Hugo CRIDLIG <80850294+hcridlig@users.noreply.github.com>
2025-02-18 16:54:25 +0000

    static front in backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from ultralytics import YOLO
 import cv2
 import numpy as np
 import base64
-from io import BytesIO
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="frontend/build", static_url_path="/")
 CORS(app)
 
 # Load the YOLO model
@@ -41,6 +41,15 @@ def predict():
 
     except Exception as e:
         return jsonify({'error': str(e)})
+
+# Serve React frontend
+@app.route('/')
+def serve_react():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_static_files(path):
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
